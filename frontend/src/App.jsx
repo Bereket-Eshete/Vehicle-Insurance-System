d5a24af0a47f23033597c821d0a30e085af123b2
Bereket-Eshete <bereketeshete63@gmail.com>
2025-03-26 12:38:34 +0300

    Initial project setup with frontend & backend.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-green-500 text-center text-6xl">
+        Vehicle Insurance System
+      </h1>
+    </>
+  );
+}
+
+export default App;
