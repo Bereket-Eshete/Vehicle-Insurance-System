006063badeb611210d395e2e58cc2273da092fef
Bereket-Eshete <bereketeshete63@gmail.com>
2025-03-26 13:52:05 +0300

    Added routes & controllers folder under the backend folder, tested server.js setup

---
+++ backend/routes/helloBackendRoutes.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { sayHello } from "../controllers/helloController.js";
+
+const router = express.Router();
+
+router.get("/", sayHello);
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,22 @@
 import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+import helloBackendRoutes from "./routes/helloBackendRoutes.js";
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log("server runnimg on port 3000....");
+//middelwares
+app.use(express.json());
+app.use(cors());
+//helemt is a security middelware that helps you protect your app by setting various Http headers
+app.use(helmet());
+app.use(morgan("dev")); //log the request
+
+app.use("/api/hello", helloBackendRoutes);
+app.listen(PORT, () => {
+  console.log("server runnimg on port " + PORT);
 });
