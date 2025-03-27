56ea9c300309a851d5b764c991362595851f37c6
Bereket-Eshete <bereketeshete63@gmail.com>
2025-03-27 21:26:02 +0300

    Setup the Database Connection

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
@@ -8,6 +9,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const prisma = new PrismaClient();
 
 //middelwares
 app.use(express.json());
@@ -17,6 +19,16 @@ app.use(helmet());
 app.use(morgan("dev")); //log the request
 
 app.use("/api/hello", helloBackendRoutes);
+
+async function testDb() {
+  try {
+    await prisma.$connect();
+    console.log("Database is Succesfully connected");
+  } catch (error) {
+    console.log("Database connection failed:", error);
+  }
+}
 app.listen(PORT, () => {
   console.log("server runnimg on port " + PORT);
+  testDb();
 });
